5ad9f9887dd6c8f8158fde274c60e67a281761fd
Nat Noordanus <n@natn.me>
2016-03-20 13:55:40 +0000

    Add Vec3I interface abstaction and extra operations

--- triangle.go
+++ triangle.go
@@ -1,22 +1,39 @@
 package geom
 
-type Triangle [3]*Vec3
+import "math"
+
+type Triangle [3]Vec3I
 
 func (t *Triangle) Area() float64 {
 	// Derive the vectors of two sides of the triangle
-	v1 := t[1].Subtract(t[0])
-	v2 := t[2].Subtract(t[0])
-	// Calculate the cross product
-	cp := v1.CrossProd(v2)
+	a, b, c := t[0], t[1], t[2]
+	v1 := b.Subtract(a)
+	v2 := c.Subtract(a)
 	// The answer is half the magnitude of the cross product
+	cp := v1.CrossProd(&v2)
 	return 0.5 * cp.Magnitude()
 }
 
-func (t *Triangle) IncludesVertex(v1 *Vec3) bool {
+func (t *Triangle) IncludesVertex(v1 Vec3I) bool {
 	for _, v2 := range t {
 		if v1 == v2 {
 			return true
 		}
 	}
 	return false
+}
+
+// Compute the normal of the plane of the triangle using the right hand rule
+func (t *Triangle) Normal() Vec3 {
+	// Derive the vectors of two sides of the triangle
+	a, b, c := t[0], t[1], t[2]
+	v1 := Vec3{b.GetX() - a.GetX(), b.GetY() - a.GetY(), b.GetZ() - a.GetZ()}
+	v2 := Vec3{c.GetX() - a.GetX(), c.GetY() - a.GetY(), c.GetZ() - a.GetZ()}
+
+	// Calculate the cross product
+	cp := v1.CrossProd(&v2)
+
+	// Normalize the cross product to arrive at the normal vector
+	l := math.Sqrt(cp.GetX()*cp.GetX() + cp.GetY()*cp.GetY() + cp.GetZ()*cp.GetZ())
+	return cp.DivideScalar(l)
 }
--- vec3.go
+++ vec3.go
@@ -2,86 +2,125 @@ package geom
 
 import "math"
 
-type Vec3 struct {
-	X float64
-	Y float64
-	Z float64
+type Vec3I interface {
+	GetX() float64
+	GetY() float64
+	GetZ() float64
+	SetX(float64)
+	SetY(float64)
+	SetZ(float64)
+	Clone() Vec3
+	Inverse() Vec3
+	Magnitude() float64
+	Normalized() Vec3
+	Add(Vec3I) Vec3
+	Sum(...Vec3I) Vec3
+	Subtract(Vec3I) Vec3
+	Multiply(Vec3I) Vec3
+	Divide(Vec3I) Vec3
+	AddScalar(n float64) Vec3
+	SubtractScalar(n float64) Vec3
+	MultiplyScalar(n float64) Vec3
+	DivideScalar(n float64) Vec3
+	Mean(...Vec3I) Vec3
+	CrossProd(Vec3I) Vec3
+	DotProd(Vec3I) float64
+	Angle(Vec3I) float64
+	LessThan(Vec3I) bool
 }
 
-func (v *Vec3) Clone() *Vec3 {
-	return &Vec3{v.X, v.Y, v.Z}
+type Vec3 struct {
+	X, Y, Z float64
 }
+
+func (v *Vec3) GetX() float64 { return v.X }
+func (v *Vec3) GetY() float64 { return v.Y }
+func (v *Vec3) GetZ() float64 { return v.Z }
+
+func (v *Vec3) SetX(x float64) { v.X = x }
+func (v *Vec3) SetY(y float64) { v.Y = y }
+func (v *Vec3) SetZ(z float64) { v.Z = z }
+
+func (v *Vec3) Clone() Vec3   { return Vec3{v.X, v.Y, v.Z} }
+func (v *Vec3) Inverse() Vec3 { return Vec3{-v.X, -v.Y, -v.Z} }
 
 func (v *Vec3) Magnitude() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
 }
 
-func (v1 *Vec3) Add(v2 Vec3) *Vec3 {
-	return &Vec3{v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z}
+func (v *Vec3) Normalized() Vec3 {
+	magnitude := v.Magnitude()
+	return Vec3{v.GetX() / magnitude, v.GetY() / magnitude, v.GetZ() / magnitude}
 }
 
-func (v1 *Vec3) Subtract(v2 *Vec3) *Vec3 {
-	return &Vec3{v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z}
+func (v1 *Vec3) Add(v2 Vec3I) Vec3 {
+	return Vec3{v1.X + v2.GetX(), v1.Y + v2.GetY(), v1.Z + v2.GetZ()}
 }
 
-func (v1 *Vec3) Multiply(n float64) *Vec3 {
-	return &Vec3{v1.X * n, v1.Y * n, v1.Z * n}
-}
-
-func (v1 *Vec3) Devide(n float64) *Vec3 {
-	return &Vec3{v1.X / n, v1.Y / n, v1.Z / n}
-}
-
-func (v1 *Vec3) Mean(vs ...*Vec3) *Vec3 {
-	acc := Vec3{0, 0, 0}
-	for _, v := range vs {
-		acc.X += v.X
-		acc.Y += v.Y
-		acc.Z += v.Z
+func (v1 *Vec3) Sum(vs ...Vec3I) (acc Vec3) {
+	acc = v1.Clone()
+	for _, v2 := range vs {
+		acc.X += v2.GetX()
+		acc.Y += v2.GetY()
+		acc.Z += v2.GetZ()
 	}
-	return acc.Devide(float64(len(vs)))
+	return
 }
 
-func (v1 *Vec3) CrossProd(v2 *Vec3) *Vec3 {
-	return &Vec3{
-		v1.Y*v2.Z - v1.Z*v2.Y,
-		v1.Z*v2.X - v1.X*v2.Z,
-		v1.X*v2.Y - v1.Y*v2.X,
+func (v1 *Vec3) Subtract(v2 Vec3I) Vec3 {
+	return Vec3{v1.X + v2.GetX(), v1.Y + v2.GetY(), v1.Z + v2.GetZ()}
+}
+
+func (v1 *Vec3) Multiply(v2 Vec3I) Vec3 {
+	return Vec3{v1.X * v2.GetX(), v1.Y * v2.GetY(), v1.Z * v2.GetZ()}
+}
+
+func (v1 *Vec3) Divide(v2 Vec3I) Vec3 {
+	return Vec3{v1.X * v2.GetX(), v1.Y * v2.GetY(), v1.Z * v2.GetZ()}
+}
+
+func (v1 *Vec3) AddScalar(n float64) Vec3 {
+	return Vec3{v1.X + n, v1.Y + n, v1.Z + n}
+}
+
+func (v1 *Vec3) SubtractScalar(n float64) Vec3 {
+	return Vec3{v1.X - n, v1.Y - n, v1.Z - n}
+}
+
+func (v1 *Vec3) MultiplyScalar(n float64) Vec3 {
+	return Vec3{v1.X * n, v1.Y * n, v1.Z * n}
+}
+
+func (v1 *Vec3) DivideScalar(n float64) Vec3 {
+	return Vec3{v1.X / n, v1.Y / n, v1.Z / n}
+}
+
+func (v1 *Vec3) Mean(vs ...Vec3I) Vec3 {
+	sum := v1.Sum(vs...)
+	return sum.DivideScalar(float64(len(vs) + 1))
+}
+
+func (v1 *Vec3) CrossProd(v2 Vec3I) Vec3 {
+	return Vec3{
+		v1.Y*v2.GetZ() - v1.Z*v2.GetY(),
+		v1.Z*v2.GetX() - v1.X*v2.GetZ(),
+		v1.X*v2.GetY() - v1.Y*v2.GetX(),
 	}
 }
 
-func (v1 *Vec3) DotProd(v2 *Vec3) (cp float64) {
-	return v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
+func (v1 *Vec3) DotProd(v2 Vec3I) (cp float64) {
+	return v1.X*v2.GetX() + v1.Y*v2.GetY() + v1.Z*v2.GetZ()
 }
 
-// Compute the normal of the face which includes by the three given points
-func TriNormal(a, b, c *Vec3) *Vec3 {
-	// Derive the vectors of two sides of the triangle
-	v1 := Vec3{b.X - a.X, b.Y - a.Y, b.Z - a.Z}
-	v2 := Vec3{c.X - a.X, c.Y - a.Y, c.Z - a.Z}
-
-	// Calculate the cross product
-	cpx := v1.Y*v2.Z - v1.Z*v2.Y
-	cpy := v1.Z*v2.X - v1.X*v2.Z
-	cpz := v1.X*v2.Y - v1.Y*v2.X
-
-	// Normalize the cross product to arrive at the normal vector
-	l := math.Sqrt(cpx*cpx + cpy*cpy + cpz*cpz)
-	return &Vec3{cpx / l,
-		cpy / l,
-		cpz / l,
-	}
-}
-
-func (v1 *Vec3) Angle(v2 *Vec3) float64 {
-	dot_prod := v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
+func (v1 *Vec3) Angle(v2 Vec3I) float64 {
+	dot_prod := v1.X*v2.GetX() + v1.Y*v2.GetY() + v1.Z*v2.GetZ()
 	v1_len := math.Sqrt(v1.X*v1.X + v1.Y*v1.Y + v1.Z*v1.Z)
-	v2_len := math.Sqrt(v2.X*v2.X + v2.Y*v2.Y + v2.Z*v2.Z)
+	v2_len := math.Sqrt(v2.GetX()*v2.GetX() + v2.GetY()*v2.GetY() + v2.GetZ()*v2.GetZ())
 	return math.Abs(math.Acos(dot_prod / (v1_len * v2_len)))
 }
 
-func (v1 *Vec3) LessThan(v2 *Vec3) bool {
-	return (v1.X < v2.X ||
-		v1.X == v2.X && (v1.Y < v2.Y ||
-			v1.Y == v2.Y && v1.Z < v2.Z))
+func (v1 *Vec3) LessThan(v2 Vec3I) bool {
+	return (v1.X < v2.GetX() ||
+		v1.X == v2.GetX() && (v1.Y < v2.GetY() ||
+			v1.Y == v2.GetY() && v1.Z < v2.GetZ()))
 }
